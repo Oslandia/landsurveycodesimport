77d808146af0f17f02571a3f97cee54a53de1533
lbartoletti <l.bartoletti@free.fr>
2019-12-17 16:05:35 +0100

    improve jxl2csv with FieldBook

--- processing/import_/trimble/jxl2csv.py
+++ processing/import_/trimble/jxl2csv.py
@@ -4,10 +4,26 @@ import csv
 import argparse
 import os
 
-def jxl2csv(jxl_path, csv_path):
+def jxl2csv(jxl_path, csv_path, isReduction=True):
     e = ElementTree.parse(jxl_path).getroot()
 
     header = ["#name", "grid_east", "grid_north", "grid_elevation", "code", "description1", "description2", "fieldbook_id", "surveymethod", "classification", "wgs84_latitude", "wgs84_longitude", "wgs84_height", "local_latitude", "local_longitude", "local_height"] # TODO: "features", "customxmlsubrecord", "layer"
+
+    if not isReduction:
+        header += ["deleted", "precision_horizontal", "precision_vertical"]
+        qc1 = ['QC1_MinimumNumberOfSatellites', 'QC1_MinGPSSVs',
+               'QC1_MinGLONASSSVs', 'QC1_MinGalileoSVs', 'QC1_MinQZSSSVs',
+               'QC1_MinBeiDouSVs', 'QC1_NumberOfSatellites', 'QC1_NumGPSSVs',
+               'QC1_NumGLONASSSVs', 'QC1_NumGalileoSVs', 'QC1_NumQZSSSVs',
+               'QC1_NumBeiDouSVs', 'QC1_RelativeDOPs', 'QC1_PDOP', 'QC1_GDOP',
+               'QC1_HDOP', 'QC1_VDOP', 'QC1_PDOP_AtStore', 'QC1_GDOP_AtStore',
+               'QC1_HDOP_AtStore', 'QC1_VDOP_AtStore',
+               'QC1_NumberOfPositionsUsed', 'QC1_HorizontalStandardDeviation',
+               'QC1_VerticalStandardDeviation']
+        header += qc1
+        qc2 = ['QC2_NumberOfSatellites', 'QC2_ErrorScale', 'QC2_VCVxx',
+               'QC2_VCVxy', 'QC2_VCVxz', 'QC2_VCVyy', 'QC2_VCVyz', 'QC2_VCVzz']
+        header += qc2
 
     def addElementToList(point, element):
         ret = ''
@@ -15,24 +31,25 @@ def jxl2csv(jxl_path, csv_path):
             ret = point.find(element).text
         except:
             pass
-    
+
         return ret
     with open(csv_path, 'w') as trimbleCSV:
         csv_writer = csv.writer(trimbleCSV, delimiter=',')
         csv_writer.writerow(header)
 
-        for point in e.findall('Reductions/Point'):
+        points = 'Reductions/Point' if isReduction else '*/PointRecord'
+        for point in e.findall(points):
             row = []
             row.append(addElementToList(point, 'Name'))
-            
-            grid = point.find('Grid')
+
+            grid = point.find('Grid') if isReduction else point.find('ComputedGrid')
             if grid: # X/Y!!!
                 row.append(grid.find('East').text)
                 row.append(grid.find('North').text)
                 row.append(grid.find('Elevation').text)
             else:
                 row += ['', '', '']
-            
+
             row.append(addElementToList(point, 'Code'))
             row.append(addElementToList(point, 'Description1'))
             row.append(addElementToList(point, 'Description2'))
@@ -55,6 +72,42 @@ def jxl2csv(jxl_path, csv_path):
                 row.append(local.find('Height').text)
             else:
                 row += ['', '', '']
+
+            if not isReduction:
+                row.append(addElementToList(point, 'Deleted'))
+
+                precision = point.find('Precision')
+                if precision:
+                    row.append(precision.find('Horizontal').text)
+                    row.append(precision.find('Vertical').text)
+                else:
+                    row += ['', '']
+
+                qualityControl1 = point.find('QualityControl1')
+                if qualityControl1:
+                    for qc in ('MinimumNumberOfSatellites', 'MinGPSSVs',
+                               'MinGLONASSSVs', 'MinGalileoSVs', 'MinQZSSSVs',
+                               'MinBeiDouSVs', 'NumberOfSatellites',
+                               'NumGPSSVs', 'NumGLONASSSVs', 'NumGalileoSVs',
+                               'NumQZSSSVs', 'NumBeiDouSVs', 'RelativeDOPs',
+                               'PDOP', 'GDOP', 'HDOP', 'VDOP', 'PDOP_AtStore',
+                               'GDOP_AtStore', 'HDOP_AtStore', 'VDOP_AtStore',
+                               'NumberOfPositionsUsed',
+                               'HorizontalStandardDeviation',
+                               'VerticalStandardDeviation'):
+                        row.append(addElementToList(qualityControl1, qc))
+                else:
+                    print("arg 1")
+                    row += [''] * len(qc1)
+
+                qualityControl2 = point.find('QualityControl2')
+                if qualityControl2:
+                    for qc in ('NumberOfSatellites', 'ErrorScale', 'VCVxx',
+                               'VCVxy', 'VCVxz', 'VCVyy', 'VCVyz', 'VCVzz'):
+                        row.append(addElementToList(qualityControl2, qc))
+                else:
+                    print("arg 2")
+                    row += [''] * len(qc2)
 
             csv_writer.writerow(row)
 
@@ -83,6 +136,12 @@ if __name__ == "__main__":
                         help='Path to the JobXML (*.jxl) file')
     parser.add_argument('csv_path', type=str,
                         help='Path to the CSV file')
+    parser.add_argument('--reduction', dest='reduction', action='store_true',
+                        help='Use only Reductions fields (common fields)')
+    parser.add_argument('--no-reduction', dest='reduction', action='store_false',
+                        help="Use only FieldBook fields (all -especially precisions' -fields and all points even deleted)")
+    parser.set_defaults(reduction=True)
+
     args = parser.parse_args()
 
     if not __checkFile(args.jxl_path):
@@ -93,4 +152,4 @@ if __name__ == "__main__":
         print("Cannot open file {}".format(args.csv_path))
         exit(1)
 
-    jxl2csv(args.jxl_path, args.csv_path)
+    jxl2csv(args.jxl_path, args.csv_path, args.reduction)
